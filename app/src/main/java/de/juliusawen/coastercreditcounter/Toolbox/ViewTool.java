7e11a9b0779e29041db9d1d3d2b53802838f0273
JuliusAwen <JuliusAwen@swbmail.de>
2018-08-20 00:31:34 +0200

    -- add ViewTool -- slightly changed visual appereance

---
+++ app/src/main/java/de/juliusawen/coastercreditcounter/Toolbox/ViewTool.java
@@ -0,0 +1,16 @@
+package de.juliusawen.coastercreditcounter.Toolbox;
+
+import android.content.Context;
+
+public abstract class ViewTool
+{
+    public static int convertDpToPx(Context context, int dp)
+    {
+        return (int) (dp * context.getResources().getDisplayMetrics().density);
+    }
+
+    public static int convertPxToDp(Context context, int px)
+    {
+        return (int) (px / context.getResources().getDisplayMetrics().density);
+    }
+}
