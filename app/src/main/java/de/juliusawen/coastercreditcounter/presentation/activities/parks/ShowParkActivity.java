edfe082591d284edd6e63b6a23595b7ad4258fe9
JuliusAwen <JuliusAwen@swbmail.de>
2018-09-07 12:22:55 +0200

    -- added android:fitsSystemWindows="true" to activity xml layouts -- reformatted xmls -- added layouts for ShowParkActivity

---
+++ app/src/main/java/de/juliusawen/coastercreditcounter/presentation/activities/parks/ShowParkActivity.java
@@ -0,0 +1,16 @@
+package de.juliusawen.coastercreditcounter.presentation.activities.parks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import de.juliusawen.coastercreditcounter.R;
+
+public class ShowParkActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_park);
+    }
+}
